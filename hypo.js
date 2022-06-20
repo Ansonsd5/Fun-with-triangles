79741aafcf82e8a4b25a0f69f88415a7a4a51efb
Ansonsd5 <anson11sd@gmail.com>
2022-06-20 18:06:39 +0530

    initial commit

--- hypo.js
+++ hypo.js
@@ -3,17 +3,30 @@ const btnHypo = document.querySelector("#btn-hypo");
 const ansHypo = document.querySelector("#output");
 
 
-function SqureofTwoSides(sideA,sideB){
-    const sum= sideA*sideA + sideB*sideB;
-    return sum;
 
+
+function SqureofTwoSides(sideA, sideB) {
+
+
+   const sum = sideA * sideA + sideB * sideB;
+   return sum;
 }
 
-function Hypotenuse(){
-   const sum = SqureofTwoSides(Number(sides[0].value),Number(sides[1].value));
-   const resultHypo = Math.sqrt(sum);
-//    console.log(resultHypo)
-   ansHypo.innerText = "The hypotenuse of the triangle is "+ resultHypo; 
-}
 
-btnHypo.addEventListener("click",Hypotenuse)
+
+
+function Hypotenuse() {
+   const a = Number(sides[0].value);
+   const b = Number(sides[1].value);
+   if ((a <= 0) || (b <= 0)) {
+      ansHypo.innerText = "INVALID INPUT";
+   } else {
+
+
+
+      const sum = SqureofTwoSides(a, b);
+      const resultHypo = Math.sqrt(sum);
+      ansHypo.innerText = "The hypotenuse of the triangle is " + resultHypo;
+   }
+}
+btnHypo.addEventListener("click", Hypotenuse)
